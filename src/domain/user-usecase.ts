43440cbcb245a294e033368183b633c8d20863c7
Hector <hector.adjakpa@gmail.com>
2024-06-14 12:12:17 +0200

    Update user-usecase.ts

--- src/domain/user-usecase.ts
+++ src/domain/user-usecase.ts
@@ -1,6 +1,6 @@
 import {DataSource, EntityNotFoundError} from "typeorm";
 import {User} from "../database/entities/user";
-import {ChangePasswordRequest, CreateUserRequest, LoginUserRequest} from "../handlers/validator/user-validator";
+import {CreateUserRequest, LoginUserRequest} from "../handlers/validator/user-validator";
 import {RoleUseCase} from "./roles-usecase";
 import {AppDataSource} from "../database/database";
 import {ClubUseCase} from "./club-usecase";
@@ -8,10 +8,6 @@ import {FormationCenterUseCase} from "./formationcenter-usecase";
 import {PlayerUseCase} from "./player-usercase";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import {ImageUseCase} from "./image-usecase";
-import {MessageUseCase} from "./message-usecase";
-import {MessageType} from "../Enumerators/MessageType";
-import {v4 as uuidv4} from 'uuid';
 
 export interface ListUserCase {
     limit: number;
@@ -28,7 +24,6 @@ export class UseruseCase {
         const query = this.db.getRepository(User).createQueryBuilder('user')
             .leftJoinAndSelect('user.role', 'role')
             .leftJoinAndSelect('user.events', 'events')
-            .where('user.deleted = false or user.deleted = null')
             .skip((listuser.page - 1) * listuser.limit)
             .take(listuser.limit);
         const [user, total] = await query.getManyAndCount();
@@ -38,11 +33,10 @@ export class UseruseCase {
         };
     }
 
-    async createUser(userData: CreateUserRequest, file: Express.Multer.File | undefined): Promise<User> {
+    async createUser(userData: CreateUserRequest): Promise<User> {
+
         const userRepository = this.db.getRepository(User);
-        const imageUseCase = new ImageUseCase(AppDataSource);
         const roleUseCase = new RoleUseCase(AppDataSource);
-        const messageUseCase = new MessageUseCase(AppDataSource);
 
         const alreadyExist = await this.getUserByEmail(userData.email);
         if (alreadyExist != null) {
@@ -57,24 +51,27 @@ export class UseruseCase {
         user.newsletter = userData.newsletter;
         user.birthDate = userData.birthDate;
         user.createDate = new Date();
-        user.firstConnection = false;
 
         const role = await roleUseCase.getRoleById(parseInt(userData.roleId));
         user.role = role;
 
         if (role.role === "CLUB") {
             const clubUseCase = new ClubUseCase(AppDataSource);
-            //@ts-ignore
-            user.club = await clubUseCase.getClubById(parseInt(userData.clubId));
-        } else if (role.role === "FORMATIONCENTER") {
+            // @ts-ignore
+            const club = await clubUseCase.getClubById(parseInt(userData.clubId));
+            user.club = club;
+        } else if (role.role === "FOMATIONCENTER") {
             const formationCenterUseCase = new FormationCenterUseCase(AppDataSource);
-            //@ts-ignore
-            user.formationCenter = await formationCenterUseCase.getFormationCenterById(parseInt(userData.formationCenterId));
+            // @ts-ignore
+            const formationCenter = await formationCenterUseCase.getFormationCenterById(parseInt(userData.formationCenterId));
+            user.formationCenter = formationCenter;
         } else if (role.role === "PLAYER") {
             const playerUseCase = new PlayerUseCase(AppDataSource);
-            //@ts-ignore
-            user.player = await playerUseCase.getPlayerById(parseInt(userData.playerId));
+            // @ts-ignore
+            const player = await playerUseCase.getPlayerById(parseInt(userData.playerId));
+            user.player = player;
         } else if (role.role === "ADMIN") {
+
         } else {
             throw new Error("this type of role is not implemented yet.");
         }
@@ -83,50 +80,29 @@ export class UseruseCase {
         user.password = await this.hashPassword(tmpPassword);
         user.matricule = await this.generateUserMatricule(user);
         user.deleted = false;
-
-        if (file != null) {
-            const uploadedImage = await imageUseCase.createImage(file);
-            if (uploadedImage) {
-                //@ts-ignore
-                user.image = uploadedImage;
-            }
-        }
-        user = await userRepository.save(user);
-
-        await messageUseCase.sendMessage(MessageType.FIRST_CONNECTION, user, tmpPassword);
-
-        return user;
+        return userRepository.save(user);
     }
 
-    async login(userRequest: LoginUserRequest): Promise<{ user: User, token?: string }> {
+    async login(userRequest: LoginUserRequest): Promise<{ user: User, token: string }> {
         const potentialUser = await this.getUserByEmail(userRequest.login);
 
         if (!potentialUser) {
             throw new Error("Email ou mot de passe incorrect");
         }
 
-        if (potentialUser.deleted) {
-            throw new Error("Votre compte est supprimé, veuillez contacter le support client");
-        }
-
         const isPasswordValid = await bcrypt.compare(userRequest.password, potentialUser.password);
 
-        if (isPasswordValid) {
+        if (!isPasswordValid) {
             throw new Error("Email ou mot de passe incorrect");
         }
 
-        if (potentialUser.a2fEnabled) {
-            await this.generateAndSendA2FCode(potentialUser.id);
-            return {user: potentialUser};
-        }
-
-        const token = await this.generateToken(potentialUser);
+        const token = this.generateToken(potentialUser);
         potentialUser.password = "{noop}"
 
         return {user: potentialUser, token};
     }
 
-    async generateToken(user: User): Promise<string> {
+    generateToken(user: User): string {
         const payload = {
             userId: user.id,
             role: user.role.role
@@ -137,19 +113,12 @@ export class UseruseCase {
         return jwt.sign(payload, secret, options);
     }
 
-    async getUserById(userId: number): Promise<User> {
+    async getUserById(userid: number): Promise<User> {
         const userRepository = this.db.getRepository(User);
-        const user = await userRepository.createQueryBuilder("user")
-            .leftJoinAndSelect("user.role", "role")
-            .leftJoinAndSelect("user.club", "club")
-            .leftJoinAndSelect("user.formationCenter", "formationCenter")
-            .leftJoinAndSelect("user.player", "player")
-            .leftJoinAndSelect("user.image", "image")
-            .where("user.id = :id", {id: userId})
-            .getOne();
+        const user = await userRepository.findOneById(userid);
 
         if (!user) {
-            throw new EntityNotFoundError(User, userId);
+            throw new EntityNotFoundError(User, userid);
         }
 
         return user;
@@ -193,7 +162,7 @@ export class UseruseCase {
         return `${roleInitial}-${firstNameInitial}${lastNameInitial}-${paddedCount}`;
     }
 
-    async getRecentUsers(): Promise<User[]> {
+    async getRecentsUsers(): Promise<User[]> {
         const userRepository = this.db.getRepository(User);
         return await userRepository.find({
             order: {
@@ -201,90 +170,6 @@ export class UseruseCase {
             },
             take: 3
         });
-    }
-
-    async changePasswordFirstConnection(userId: number, newPassword: string): Promise<User> {
-        const userRepository = this.db.getRepository(User);
-        const user = await userRepository.findOne({
-            where: {id: userId}
-        });
-
-        if (!user) {
-            throw new Error("Utilisateur inconnu");
-        }
-        if (user.firstConnection) {
-            throw new Error("Mot de passe déjà changé");
-        }
-        user.password = await this.hashPassword(newPassword);
-        user.firstConnection = true;
-
-        return await userRepository.save(user);
-    }
-
-    async desactivateUserById(userId: number): Promise<User> {
-        const userRepository = this.db.getRepository(User);
-        const user = await this.getUserById(userId);
-
-        if (!user) {
-            throw new Error("Utilisateur inconnu");
-        }
-
-        user.deleted = true;
-        return await userRepository.save(user);
-    }
-
-    async changePassword(userId: number, changePassword: ChangePasswordRequest): Promise<User> {
-
-        const userRepository = this.db.getRepository(User);
-        const user = await this.getUserById(userId);
-
-        if (!user) {
-            throw new Error("Utilisateur inconnu");
-        }
-
-        const oldPasswordMatch = await bcrypt.compare(changePassword.oldPassword, user.password);
-
-        if (!oldPasswordMatch) {
-            throw new Error("Ancien mot de passe invalide");
-        }
-
-        user.password = await this.hashPassword(changePassword.newPassword);
-        return await userRepository.save(user);
-    }
-
-    async generateAndSendA2FCode(userId: number) {
-        const userRepository = this.db.getRepository(User);
-        const messageUseCase = new MessageUseCase(AppDataSource);
-        const user = await this.getUserById(userId);
-
-        if (!user) {
-            throw new Error("Utilisateur inconnu");
-        }
-
-        const a2fCode = uuidv4().slice(0, 6); // Generate a short unique code
-        user.a2fCode = a2fCode;
-        user.a2fCodeCreatedAt = new Date();
-
-        await userRepository.save(user);
-        await messageUseCase.sendMessage(MessageType.A2F_CODE, user, a2fCode);
-    }
-
-    async validateA2FCode(userId: number, code: string): Promise<{ token: string }> {
-        const user = await this.getUserById(userId);
-        if (!user || !user.a2fCode || !user.a2fCodeCreatedAt) {
-            throw new Error("Un problème est survenu, veuillez réessayer plus tard");
-        }
-
-        const now = new Date();
-        const codeCreationTime = new Date(user.a2fCodeCreatedAt);
-        const timeDiff = (now.getTime() - codeCreationTime.getTime()) / 1000 / 60;
-
-        if (timeDiff > 15 || user.a2fCode !== code) {
-            throw new Error("Code A2F invalide ou expiré");
-        }
-
-        const token = await this.generateToken(user);
-        return {token};
     }
 }
 
